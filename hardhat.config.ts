ccf9b4abc939cd7e41ba74d643cd3554bd4fedd4
armariya <arm.armariya@gmail.com>
2021-02-18 17:53:06 +0700

    chore: add gitignore and openzeppelin contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,6 +19,6 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
-  solidity: "0.7.3",
+  solidity: "0.8.1",
 };
 
