2074617b20209d805e3f44c9b0f98312bfdb908a
armariya <arm.armariya@gmail.com>
2021-02-18 17:54:43 +0700

    feat: :sparkles:  add important interface to use

--- contracts/IVault.sol
+++ contracts/IVault.sol
@@ -1,18 +1,18 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
 interface IVault {
-    // Returns the unwrapped native token address that the Vault takes as deposit.
-    function token() external view returns (address);
+    // // Returns the unwrapped native token address that the Vault takes as deposit.
+    // function token() external view returns (address);
 
-    // Returns the vault’s wrapped token name as a string, e.g. “yearn Dai Stablecoin".
-    function name() external view returns (string memory);
+    // // Returns the vault’s wrapped token name as a string, e.g. “yearn Dai Stablecoin".
+    // function name() external view returns (string memory);
 
-    // Returns the vault’s wrapped token symbol as a string, e.g. “yDai”.
-    function symbol() external view returns (string memory);
+    // // Returns the vault’s wrapped token symbol as a string, e.g. “yDai”.
+    // function symbol() external view returns (string memory);
 
-    // Returns the amount of decimals for this vault’s wrapped token as a uint8.
-    function decimals() external view returns (uint8);
+    // // Returns the amount of decimals for this vault’s wrapped token as a uint8.
+    // function decimals() external view returns (uint8);
 
     // The calculation is nativeTokenBalance / yTokenTotolSupply
     function getPricePerFullShare() external view returns (uint256);
