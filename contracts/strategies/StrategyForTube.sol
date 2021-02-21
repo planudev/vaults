7b7ebe3c585663d3f15e0b8d247756babffd4c32
Ugrit Wongkham <6243415-tarOzone@users.noreply.gitlab.com>
2021-02-21 15:13:42 +0700

    feat: add ForTube strategy

--- contracts/strategies/StrategyForTube.sol
+++ contracts/strategies/StrategyForTube.sol
@@ -2,78 +2,99 @@
 pragma solidity ^0.8.0;
 
 import "./StrategyStorage.sol";
+import "../interfaces/IBank.sol";
 import "../interfaces/IBEP20.sol";
 import "../interfaces/IFToken.sol";
 import "../interfaces/IStrategy.sol";
 
 contract StrategyStorageForTube is StrategyStorage {
+
+    address internal _bankAddress = address(0x0cEA0832e9cdBb5D476040D58Ea07ecfbeBB7672);
     
-    function _deposit(address underlying, address vToken, uint256 _amount) internal {
+    function _deposit(address underlying, uint256 amount) internal {
+        IBank(_bankAddress).deposit(underlying, amount);
     }
 
-    function _redeem(address vToken, uint256 amount) internal returns (uint) {
-        return 0;
+    function _redeem(address underlying, uint256 amount) internal returns (uint) {
+        IBank(_bankAddress).withdraw(underlying, amount);
+        return amount;
     }
 
     function _balanceOfUnderlying(address underlying) internal view returns (uint256) {
         return IBEP20(underlying).balanceOf(address(this));
     }
 
-    function _balanceOfVToken(address vToken) internal view returns (uint256) {
-        return 0;
+    function _balanceOfFToken(address fToken) internal view returns (uint256) {
+        return IFToken(fToken).balanceOf(address(this));
     }
 
-    function _balanceOfVTokenInUnderlying(address vToken) internal view returns (uint256) {
-        return 0;
+    function _balanceOfFTokenInUnderlying(address fToken) internal view returns (uint256) {
+        uint256 balanceOfFToken = _balanceOfFToken(fToken);
+        if (balanceOfFToken > 0)
+            balanceOfFToken = balanceOfFToken * _getExchangeRate(fToken) / 1e18;
+        return balanceOfFToken;
     }
 
     function _underlyingDecimals(address underlying) internal view returns (uint) {
-        return 0;
+        return IBEP20(underlying).decimals();
     }
 
-    function _vTokenDecimals(address vToken) internal view returns (uint) {
-        return 0;
+    function _fTokenDecimals(address fToken) internal view returns (uint) {
+        return IFToken(fToken).decimals();
     }
 
-    function _getExchangeRate(address vToken) internal view returns (uint256) {
-        return 0;
+    function _getExchangeRate(address fToken) internal view returns (uint256) {
+        return IFToken(fToken).exchangeRateStored();
     }
 
-    function _getApy() internal pure returns (uint256) {
-        // [TODO] #2: calculate to get APY in ForTube. 
-        return 0;
+    function _getApy(address fToken) internal view returns (uint256) {
+        return IFToken(fToken).APY();
     }
 }
+
 
 
 contract StrategyForTube is StrategyStorageForTube, IStrategy {
 
     address internal _want;
-    address internal _vToken;
+    address internal _fToken;
 
     function want() external override view returns (address) {
         return _want;
     }
 
     function deposit() external override {
+        uint256 balanceOfUnderlying = _balanceOfUnderlying(_want);
+        if (balanceOfUnderlying > 0) {
+            _deposit(_want, balanceOfUnderlying);
+        }
     }
 
     function withdraw(uint256 amount) external override {
+        _redeem(_want, amount);
+        _sendToVaultWithFee(_want, amount);
     }
 
     function withdrawAll() external override returns (uint256) {
-        return 0;
+        uint256 balanceOfFToken = _balanceOfFToken(_fToken);
+        if (balanceOfFToken > 0) {
+            _redeem(_want, balanceOfFToken);
+        }
+
+        uint256 balanceOfUnderlying = _balanceOfUnderlying(_want);
+        _sendToVault(_want, balanceOfUnderlying);
+        return balanceOfUnderlying;
     }
 
     function balanceOf() external override view returns (uint256) {
-        return 0;
+        return _balanceOfUnderlying(_want) + _balanceOfFTokenInUnderlying(_fToken);
     }
 
     function withdrawalFee() external override view returns (uint256) {
-        return 0;
+        return _withdrawalFee;
     }
     
-    function annualPercentageYield() external override pure returns (uint256) {
-        return 0;
+    function annualPercentageYield() external override view returns (uint256) {
+        return _getApy(_fToken);
     }
 }
